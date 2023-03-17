d57ec479bca66d988400c85a2962e6be89561536
unknown <ericknevesea@gmail.com>
2023-03-17 12:35:31 -0300

    CPF verifica

--- processa-formulario.php
+++ processa-formulario.php
@@ -1,12 +1,25 @@
 <?php
-    // $inst = $_POST['instituicao'];
-    // // $gest = $_POST['gestor'];
-    // // $pac = $_POST['paciente'];
-    // // $cpf = $_POST['cpf'];
-    // // // $cpfNumberCheck = $_POST['inst'];
-    // // // $cpfQntCheck = $_POST['inst'];
-    // // // echo $nome;
-    // // echo $inst . "<br>" . $gest . "<br>" . $pac . "<br>" . $cpf;
-    $btn = $_POST['cpf'];
-    echo $btn;
+    //declarando as variáveis.
+
+    $est = $_POST['estados'];
+    $cid = $_POST['cidades'];
+    $inst = $_POST['instituicao'];
+    $gest = $_POST['gestor'];
+    $pac = $_POST['paciente'];
+    $cpf = $_POST['cpf'];
+    $cpfNumberCheck = is_numeric($cpf);
+    $cpfQntCheck = strlen($cpf);
+
+    
+
+    //iniciando lógica para verificar o CPF.
+
+    if ($cpfNumberCheck == 1 and $cpfQntCheck == 11){
+    
+        echo json_encode("Estado: " . $est . "<br>" . "Cidade: " . $cid . "<br>" . "Instituicao: " . $inst . "<br>" . "Gestor: " . 
+        $gest . "<br>" . "Paciente: " . $pac . "<br>" . "CPF: " . $cpf . "<br>");
+
+    } else {
+        echo "Erro!";
+    };   
 ?>
