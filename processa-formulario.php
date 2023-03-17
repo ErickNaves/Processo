268d10bb72854c7d14be7c470993c3f8ec7c8683
Erick Neves <ericknevesea@gmail.com>
2023-03-17 11:13:44 -0300

    init

---
+++ processa-formulario.php
@@ -0,0 +1,12 @@
+<?php
+    // $inst = $_POST['instituicao'];
+    // // $gest = $_POST['gestor'];
+    // // $pac = $_POST['paciente'];
+    // // $cpf = $_POST['cpf'];
+    // // // $cpfNumberCheck = $_POST['inst'];
+    // // // $cpfQntCheck = $_POST['inst'];
+    // // // echo $nome;
+    // // echo $inst . "<br>" . $gest . "<br>" . $pac . "<br>" . $cpf;
+    $btn = $_POST['cpf'];
+    echo $btn;
+?>
